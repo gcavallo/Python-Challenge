ae14f171ea6538bef026f1d1e3441194132eef28
Gabriel Cavallo <gabrielcavallo@mail.com>
2015-07-19 07:01:10 -0400

    Add challenge 5

---
+++ 5.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import urllib2, pickle, re
+
+source = pickle.loads(urllib2.urlopen('http://www.pythonchallenge.com/pc/def/banner.p').read())
+text = ''.join(x*y for i in source for x,y in i)
+
+print re.sub("(.{95})", "\\1\n", text, 0, re.DOTALL)
