a836f142259c8bea9e236f0b6a52c674f762eab6
Gabriel Cavallo <gabrielcavallo@mail.com>
2015-07-19 09:37:23 -0400

    Use zip instead of extracted folder

--- 6.py
+++ 6.py
@@ -7,10 +7,10 @@ nothing = "90052"
 comments = []
 
 while nothing.isdigit():
-	content = open('channel/' + nothing + '.txt').read()
-	print content
 	zf = zipfile.ZipFile('channel.zip', 'r')
-	info = zf.getinfo(nothing + '.txt')
+	content = zf.open('{0}.txt'.format(nothing)).read()
+	print content
+	info = zf.getinfo('{0}.txt'.format(nothing))
 	comments.append(info.comment)
 	try:
 		nothing = re.search('(?<=Next nothing is )(\d+)', content).group(0)
