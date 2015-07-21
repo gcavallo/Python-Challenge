f4342dc83c7b63620289ebf1130af1bdea129cfd
Gabriel Cavallo <gabrielcavallo@mail.com>
2015-07-21 10:58:06 -0400

    Add challenge 7

---
+++ 7.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+import re
+
+im = Image.open('oxygen.png')
+im = im.crop((4,43,608,44))
+
+text = ''.join(chr(im.getpixel((i, 0))[0]) for i in range(0, im.size[0], 7))
+print text
+print ''.join(chr(int(i)) for i in re.findall('\d+', text, re.DOTALL))
