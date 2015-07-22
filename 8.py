36e4deb89709716204a33a9bb2f34bc02ec06f7c
Gabriel Cavallo <gabrielcavallo@mail.com>
2015-07-22 13:30:23 -0400

    Add challenge 8

---
+++ 8.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import urllib2, re, bz2
+
+data = urllib2.urlopen('http://www.pythonchallenge.com/pc/def/integrity.html').read()
+
+print '\n'.join(bz2.decompress(i.decode('string_escape')) for i in re.findall('\w{2}: \'(.*?)\'', data))
